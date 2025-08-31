d50204dc0cff5ba08b6ad29bf347724d47c22bf8
carlynorama <carlynorama@gmail.com>
2025-08-30 18:32:21 -0700

    found the local swift-backtrace

--- README.md
+++ README.md
@@ -5,10 +5,9 @@ https://doineedkubernetes.com
 
 UNDER CONSTRUCTION. 
 
+Note pulling from slim and moving from local computer behaved the same. 
 
-
-
-
+Using the container plugin also did not catch the back trace. 
 
 
 
--- buildNRun.sh
+++ buildNRun.sh
@@ -3,8 +3,10 @@
 DEFAULT_CONFIG_VALUE="release"
 CONFIGURATION="${1:-$DEFAULT_CONFIG_VALUE}"
 
-swift build -c $CONFIGURATION --swift-sdk x86_64-swift-linux-musl
 mkdir -p HelloWrapper/binary/
+cp /Users/$USER/Library/org.swift.swiftpm/swift-sdks/swift-6.1.2-RELEASE_static-linux-0.0.1.artifactbundle/swift-6.1.2-RELEASE_static-linux-0.0.1/swift-linux-musl/musl-1.2.5.sdk/x86_64/usr/libexec/swift/linux-static/swift-backtrace-static HelloWrapper/binary/swift-backtrace
+
+swift build -c $CONFIGURATION --swift-sdk x86_64-swift-linux-musl
 cp .build/x86_64-swift-linux-musl/$CONFIGURATION/hello-world HelloWrapper/binary/hello-world
 
 podman build -f HelloWrapper/Containerfile -t wrapped-hello HelloWrapper/
